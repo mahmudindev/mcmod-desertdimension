6754315e9b397609f41f0149d7479e6d1ac66934
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2026-05-03 01:09:11 +0700

    Updates to 26.1

--- common/src/main/java/com/github/mahmudindev/mcmod/desertdimension/DesertDimension.java
+++ common/src/main/java/com/github/mahmudindev/mcmod/desertdimension/DesertDimension.java
@@ -1,7 +1,14 @@
 package com.github.mahmudindev.mcmod.desertdimension;
 
+import com.github.mahmudindev.mcmod.desertdimension.platform.Services;
+import com.github.mahmudindev.mcmod.desertdimension.platform.services.IPlatformHelper;
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
 public final class DesertDimension {
     public static final String MOD_ID = "desertdimension";
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public static final IPlatformHelper PLATFORM = Services.PLATFORM;
 
     public static void init() {
         // Write common init code here.
